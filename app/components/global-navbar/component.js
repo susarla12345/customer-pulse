e969da8d626f79b1a0b0b7f4a11adaa45ba90854
mallikarjun susarla <mallikarjun.susarla@freshworks.com>
2020-10-26 20:59:36 +0530

    add navbar svg icons

--- app/components/global-navbar/component.js
+++ app/components/global-navbar/component.js
@@ -6,21 +6,25 @@ const TABS = [{
     name: 'Pulse',
     route: 'pulse',
     beta: false,
+    icon: 'pulse',
 },
 {
     name: 'Feedback',
     route: 'feedback',
     beta: false,
+    icon: 'feedback'
 },
 {
     name: 'Metrics',
     route: 'metrics',
     beta: true,
+    icon: 'metrics'
 },
 {
     name: 'Impact',
     route: 'impact',
     beta: true,
+    icon: 'impact'
 }]
 
 export default Ember.Component.extend({
